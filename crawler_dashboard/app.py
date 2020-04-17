ee9ef6530aa1800625f4c5040570c1e109f388fa
azkafr92 <azkafr92@gmail.com>
2020-04-17 06:04:15 +0000

    add pipfile

--- crawler_dashboard/app.py
+++ crawler_dashboard/app.py
@@ -25,7 +25,8 @@ from get_dataset import get_dataset
 server = Flask(__name__)
 app = dash.Dash(__name__,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-                external_stylesheets=[dbc.themes.BOOTSTRAP])
+                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                server=server)
 app.config['suppress_callback_exceptions']=True
 
 website_and_channel_options = {
